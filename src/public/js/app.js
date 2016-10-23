8ac3005e57a9e4d61bb36d31caed07f79ab39da7
klvngnn <klvngnn@gmail.com>
2016-10-23 06:36:12 -0700

    add animation

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-  $('#scan').click(function () {
-    $('.millie').addClass('scanning');
-    setTimeout(function () {
-     $('.millie').removeClass('scanning');
-   }, 3 * 1000);
-  });
+  $('.cam').on("click", function(){
+  	
+
+  	})
 });
+
+
+
+
