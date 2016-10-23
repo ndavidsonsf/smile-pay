c0a06af2c7e518b00e116d59ec07d2096405d25a
Luis De Castro <luisdecastro510@gmail.com>
2016-10-23 07:09:09 -0700

    add camera and modal

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,4 +5,22 @@ $(document).ready(function () {
      $('.millie').removeClass('scanning');
    }, 3 * 1000);
   });
+  $('.cam').click(function () {
+    take_snapshot()
+    $('#myModal').modal()
+  })
+  Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+  });
+
+  Webcam.attach( '#my_camera' );
 });
+
+function take_snapshot() {
+    Webcam.snap( function(data_uri) {
+        document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+    } );
+}
